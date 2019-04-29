971e6b519ab34b7cb19cc4faeed29f029d00f9e1
Alex <zhuruihe123@qq.com>
2019-04-29 15:32:22 +0800

    Rewriting lenet and train.py to make a deeper understanding.

--- net/lenet.py
+++ net/lenet.py
@@ -1,37 +1,35 @@
-# import the necessary packages
 from keras.models import Sequential
-from keras.layers.convolutional import Conv2D
-from keras.layers.convolutional import MaxPooling2D
-from keras.layers.core import Activation
-from keras.layers.core import Flatten
-from keras.layers.core import Dense
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Activation, Flatten, Dense
 from keras import backend as K
- 
+
 class LeNet:
     @staticmethod
     def build(width, height, depth, classes):
         # initialize the model
         model = Sequential()
-        inputShape = (height, width, depth)
+        input_shape = (height, width, depth)
         # if we are using "channels last", update the input shape
-        if K.image_data_format() == "channels_first":   #for tensorflow
-            inputShape = (depth, height, width)
-        # first set of CONV => RELU => POOL layers
-        model.add(Conv2D(20, (5, 5),padding="same",input_shape=inputShape))
+        if K.image_data_format() == "channels_first":
+            # for tensorflow 
+            input_shape = (depth, height, width)
+        # first set of CONV --> RELU --> POOL layers
+        model.add(Conv2D(20, (5, 5), padding="same", input_shape=input_shape))
         model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-        #second set of CONV => RELU => POOL layers
-        model.add(Conv2D(50, (5, 5), padding="same"))
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+        # second set fo CONV --> RELU --> POOL layers
+        model.add(Conv2D(50, (5,5), padding="same"))
         model.add(Activation("relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-        # first (and only) set of FC => RELU layers
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
+        # first and only FC layer --> RELU
         model.add(Flatten())
         model.add(Dense(500))
         model.add(Activation("relu"))
-
+        
         # softmax classifier
         model.add(Dense(classes))
         model.add(Activation("softmax"))
-
-        # return the constructed network architecture
+        
         return model
+        
+
